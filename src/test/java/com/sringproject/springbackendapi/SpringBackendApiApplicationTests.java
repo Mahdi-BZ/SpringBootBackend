1deb09f88a4bd020ef43813e071ab7c9c217c1d7
mahdi-BZ <benzinoubamahdi@gmail.com>
2022-11-14 14:49:32 +0100

    tests are working

--- src/test/java/com/sringproject/springbackendapi/SpringBackendApiApplicationTests.java
+++ src/test/java/com/sringproject/springbackendapi/SpringBackendApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.sringproject.springbackendapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class SpringBackendApiApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
